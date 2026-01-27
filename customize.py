849995319b74321ac5badc714aba630ab09c5a25
Aditya Upadhayay <adityaupadhayay066@gmail.com>
2026-01-27 10:02:02 +0530

    Add styled multi-line Matplotlib plot

---
+++ customize.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5,6,7,8,9,10]
+y1 = [2,5,3,8,2,11,15,6,8,3]
+y2 = [6,7,2,12,1,14,12,10,4,2]
+plt.plot(x,y1, label = "Line 1")
+plt.plot(x,y2, label="Line 2")
+plt.plot(x,y1,label = "Line 1", color = "red", marker = ".", linestyle = "dashed" , linewidth = 2, markersize = 12)
+plt.plot(x,y2,label = "Line 1", color = "green", marker = "o", linestyle = "dashed" , linewidth = 2, markersize = 12)
+plt.title("My sample Graph ")
+plt.legend()
+plt.show()
+# print (plt.style.available)
